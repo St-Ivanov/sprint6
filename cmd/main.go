38c0f68e614f4635d2c18bc4f55f12098bf5b56b
Ivanov, Stepan Ivanov <si655386@gmail.com>
2026-02-14 23:23:45 +0300

    sprint6_v1.0.2

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/St-Ivanov/sprint6/internal/server"
 )
 
 func main() {
-	file, err := os.OpenFile("../log/info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	err := os.Chdir("..")
+	if err != nil {
+		log.Fatal(err)
+	}
+	curDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.OpenFile(filepath.Join(curDir, "/log/info.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 		return
