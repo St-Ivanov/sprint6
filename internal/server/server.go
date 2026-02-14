37fc07f019e38a23f1cf22ca053609db2d1df09c
Ivanov, Stepan Ivanov <si655386@gmail.com>
2026-02-14 23:46:39 +0300

    sprint6_v1.0.7

--- internal/server/server.go
+++ internal/server/server.go
@@ -20,13 +20,9 @@ func NewHttpServer(loger *log.Logger) *HttpServer {
 	r.Get("/", handlers.MainHandler)
 	r.Post("/upload", handlers.UploadHandler)
 
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		loger.Fatal(err)
-	}
 	serv := HttpServer{
 		Loger: loger, Serv: &http.Server{
-			Addr: ":8070", Handler: r, ErrorLog: loger, ReadTimeout: time.Duration(5) * time.Second, WriteTimeout: time.Duration(10) * time.Second, IdleTimeout: time.Duration(15) * time.Second,
+			Addr: ":8080", Handler: r, ErrorLog: loger, ReadTimeout: time.Duration(5) * time.Second, WriteTimeout: time.Duration(10) * time.Second, IdleTimeout: time.Duration(15) * time.Second,
 		},
 	}
 	return &serv
