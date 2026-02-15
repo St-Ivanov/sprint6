6bf3202c679d211395f5ec8abc9aee503c6d7c4c
Ivanov, Stepan Ivanov <si655386@gmail.com>
2026-02-15 22:32:07 +0300

    v1.0.22

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -42,12 +42,10 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	filePath := "data/" + time.Now().UTC().Format("02012006_150405") + formatFile
 
-	os.Create(filePath)
-
-	fileNew, err := os.Create(filePath)
-
+	fileNew, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		f, _ := os.Getwd()
+		http.Error(w, err.Error()+" "+f, http.StatusInternalServerError)
 		return
 	}
 	defer fileNew.Close()
